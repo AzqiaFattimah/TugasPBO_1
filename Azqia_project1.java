5b3b1985bbb94dd6b402f563e5bb7ed72496c6b2
AzqiaFattimah <azfatt10zahrauuf10@gmail.com>
2024-09-01 20:47:26 +0700

    Add files via upload

---
+++ Azqia_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.azqia_project1;
+
+/**
+ *
+ * @author loq
+ */
+public class Azqia_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Azqia!");
+        System.out.println("NPM : 2340506067!");
+        System.out.println("Alamat : Magelang!");
+        System.out.println("No_HP : 08132412632!");
+    } 
+}
